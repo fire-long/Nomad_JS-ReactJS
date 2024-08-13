85406f9caa4543da9f2e88f92b6bb3300dfe107d
jinaen <fire_long346@naver.com>
2024-08-13 15:26:33 +0900

    first commit

---
+++ Momentum/app.js
@@ -0,0 +1,13 @@
+//alert("hi");
+
+//console 창에 기록을 남김.
+//'과 " 모두 문자열로 사용 가능하지만, 둘을 교차해서 쓸 수는 없음.
+//console.log("lalalalala");
+
+//const는 상수(constant) '
+const a = 5;
+const b = 2;//!="2"
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
